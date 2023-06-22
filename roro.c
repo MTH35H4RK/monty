26847dc9d652b14258a21745a6bd7796d4b1fd3d
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-06-22 15:52:36 -0700

    fix betty

--- roro.c
+++ roro.c
@@ -8,20 +8,21 @@
 
 void rotl(stack_t **stack, unsigned int line_number)
 {
-        (void)line_number;
+	(void)line_number;
 
-    if (*stack != NULL && (*stack)->next != NULL)
-    {
-        stack_t *last = *stack;
-        while (last->next != NULL)
-            last = last->next;
+	if (*stack != NULL && (*stack)->next != NULL)
+	{
+		stack_t *last = *stack;
 
-        last->next = *stack;
-        (*stack)->prev = last;
-        *stack = (*stack)->next;
-        (*stack)->prev->next = NULL;
-        (*stack)->prev = NULL;
-    }
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = *stack;
+		(*stack)->prev = last;
+		*stack = (*stack)->next;
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
 }
 /**
  * rotr - rotates the stack to the bottom
@@ -32,20 +33,20 @@ void rotl(stack_t **stack, unsigned int line_number)
 
 void rotr(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp;
-        (void)line_number;
+	stack_t *temp;
+	(void)line_number;
 
-        if (*stack && (*stack)->next)
-        {
-                temp = *stack;
+	if (*stack && (*stack)->next)
+	{
+		temp = *stack;
 
-                while (temp->next != NULL)
-                        temp = temp->next;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-                temp->prev->next = NULL;
-                temp->prev = NULL;
-                temp->next = *stack;
-                (*stack)->prev = temp;
-                *stack = temp;
-        }
+		temp->prev->next = NULL;
+		temp->prev = NULL;
+		temp->next = *stack;
+		(*stack)->prev = temp;
+		*stack = temp;
+	}
 }
