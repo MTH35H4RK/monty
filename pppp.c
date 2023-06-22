6c392a14c36b12bb6c5a8862f24ab6897eff8a4e
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-06-22 15:58:40 -0700

    fix betty

--- pppp.c
+++ pppp.c
@@ -12,17 +12,16 @@
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-        /* Print all the values on the stack, starting from the top*/
-        stack_t *current;
+	stack_t *current;
 
-        (void)line_number;
+	(void)line_number;
 
-        current = *stack;
-        while (current)
-        {
-                printf("%d\n", current->n);
-                current = current->next;
-        }
+	current = *stack;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 /**
  * pop - removes the top element of the stack
@@ -32,17 +31,18 @@ void pall(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp = *stack;
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	stack_t *temp = *stack;
 
-        *stack = (*stack)->next;
-        if (*stack != NULL)
-                (*stack)->prev = NULL;
-        free(temp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
 }
 /**
  * pint - prints the value at the top of the stack, followed by a new line.
@@ -53,13 +53,13 @@ void pop(stack_t **stack, unsigned int line_number)
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%d\n", (*stack)->n);
+	printf("%d\n", (*stack)->n);
 }
 /**
  * push - Push a value onto the stack
@@ -73,40 +73,35 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-        int n;
-        stack_t *new_node;
-        /* Get the integer value from the next token*/
-        char *value;
+	int n;
+	stack_t *new_node;
 
-        value = strtok(NULL, " \n");
+	char *value;
 
-        if (!value)
-        {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	value = strtok(NULL, " \n");
 
-        /* Convert the value to an integer*/
-        n = atoi(value);
+	if (!value)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        /* Create a new node*/
-        new_node = malloc(sizeof(stack_t));
+	n = atoi(value);
 
-        if (!new_node)
-        {
-                fprintf(stderr, "Error: malloc failed\n");
-                exit(EXIT_FAILURE);
-        }
+	new_node = malloc(sizeof(stack_t));
 
-        /* Set the value of the new node*/
-        new_node->n = n;
-        new_node->prev = NULL;
-        new_node->next = *stack;
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-        /* Update the previous pointer of the existing top node, if any */
-        if (*stack)
-                (*stack)->prev = new_node;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-        /* Update the stack pointer to point to the new node */
-        *stack = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
 }
