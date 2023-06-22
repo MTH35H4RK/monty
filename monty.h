d89e04749c575145720cdd59db15f1ba75466d5d
MTH35H4RK <switchayman@gmail.com>
2023-06-22 00:27:58 -0700

    updates to the header file

--- monty.h
+++ monty.h
@@ -3,19 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* Data structures */
-
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
 typedef struct stack_s
 {
         int n;
@@ -23,14 +12,6 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
 typedef struct instruction_s
 {
         char *opcode;
@@ -38,6 +19,7 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Function prototypes */
+void execute_instruction(char *line, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
